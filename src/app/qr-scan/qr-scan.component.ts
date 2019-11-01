87dffdee1bcab4e1ef47dbe67902705847df760b
zbynekstrnad <zbynek.strnad@evrythng.com>
2019-11-01 14:57:31 +0000

    QR redirection

--- src/app/qr-scan/qr-scan.component.ts
+++ src/app/qr-scan/qr-scan.component.ts
@@ -2,8 +2,8 @@ import {Component, NgZone, OnInit} from '@angular/core';
 import * as evrythng from 'evrythng';
 import ScanThng from 'scanthng/src/index'
 
-const APPLICATION_API_KEY = 'OFRS90So0W5xROeSAPrt2JG1HdPxVii8oYXCnLiHs1eGNFC0lWntEU9t4Tiv81GgH8Q1b2v0u7AU47ZD';
-
+// const APPLICATION_API_KEY = 'OFRS90So0W5xROeSAPrt2JG1HdPxVii8oYXCnLiHs1eGNFC0lWntEU9t4Tiv81GgH8Q1b2v0u7AU47ZD';
+const APPLICATION_API_KEY = 'VAujQcbk5xksz33xA38qadH2wiEskyPbP9l199AhLfpoGKTD3B0918nHlAQQ1Ui5sWvJLo9xW36vz6jG';
 
 global['evrythng'] = evrythng;
 
@@ -45,6 +45,7 @@ export class QrScanComponent implements OnInit {
         containerId: 'stream_container',
       })
         .then((res) => {
+          console.log(res);
           if (!res.length) {
             console.log('No results');
             return;
@@ -53,9 +54,13 @@ export class QrScanComponent implements OnInit {
           this.result = res;
 
           // Raw URL
-        //   const url = (res.length && res[0].results.length)
-        //     ? res[0].results[0].redirections[0]
-        //     : res[0].meta.value;
+          const url = (res.length && res[0].results.length)
+            ? res[0].results[0].redirections[0]
+            : res[0].meta.value;
+
+          console.log(url);
+
+          window.location = url;
         })
         .catch(console.log);
     // })
